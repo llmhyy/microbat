ac826ffdbd5217ad44912a641811a3d8b22d0f91
Yun Lin <llmhyy@gmail.com>
2016-09-06 12:57:57 +0800

    a better structure

--- microbat/src/evaluation/microbat/evaluation/handler/EvaluationAllHandler.java
+++ microbat/src/evaluation/microbat/evaluation/handler/EvaluationAllHandler.java
@@ -9,24 +9,58 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 
-import microbat.evaluation.junit.TestCaseEvaluator;
+import microbat.evaluation.io.ExcelReporter;
+import microbat.evaluation.io.IgnoredTestCaseFiles;
+import microbat.evaluation.junit.ParsedTrials;
+import microbat.evaluation.junit.TestCaseAnalyzer;
+import microbat.util.JavaUtil;
+import microbat.util.Settings;
 
 public class EvaluationAllHandler extends AbstractHandler {
 
+	private IgnoredTestCaseFiles ignoredTestCaseFiles;
+	private ParsedTrials parsedTrials;
+	
+	private int trialNumPerTestCase = 3;
+	private double[] unclearRates = {0, 0.005, 0.01, 0.05, 0.1, -1};
+	
+	
+	private boolean isLimitTrialNum = false;
+	private int optionSearchLimit = 100;
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		ignoredTestCaseFiles = new IgnoredTestCaseFiles();
+		parsedTrials = new ParsedTrials();
 		
 		Job job = new Job("Do evaluation") {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				TestCaseEvaluator parser = new TestCaseEvaluator();
+				
+				TestCaseAnalyzer analyzer = new TestCaseAnalyzer();
+				
 				try {
-					parser.runEvaluation(TestCaseEvaluator.ALL);
+					ExcelReporter reporter = new ExcelReporter(Settings.projectName, unclearRates);
+					IPackageFragmentRoot testRoot = JavaUtil.findTestPackageRootInProject();
+					
+					for(IJavaElement element: testRoot.getChildren()){
+						if(element instanceof IPackageFragment){
+							analyzer.runEvaluation((IPackageFragment)element, reporter, isLimitTrialNum, 
+									ignoredTestCaseFiles, parsedTrials, trialNumPerTestCase, unclearRates, optionSearchLimit);				
+						}
+					}
 				} catch (JavaModelException | IOException e) {
 					e.printStackTrace();
 				}
+				
+				
+				
 				return Status.OK_STATUS;
 			}
 		};
--- microbat/src/evaluation/microbat/evaluation/handler/EvaluationTestCaseHandler.java
+++ microbat/src/evaluation/microbat/evaluation/handler/EvaluationTestCaseHandler.java
@@ -11,18 +11,40 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.JavaModelException;
 
-import microbat.evaluation.junit.TestCaseEvaluator;
+import microbat.evaluation.io.ExcelReporter;
+import microbat.evaluation.io.IgnoredTestCaseFiles;
+import microbat.evaluation.junit.ParsedTrials;
+import microbat.evaluation.junit.TestCaseAnalyzer;
+import microbat.util.Settings;
 
 public class EvaluationTestCaseHandler extends AbstractHandler {
 
+	private IgnoredTestCaseFiles ignoredTestCaseFiles;
+	private ParsedTrials parsedTrials;
+	
+	private int trialNumPerTestCase = 3;
+	private double[] unclearRates = {0, 0.005, 0.01, 0.05, 0.1, -1};
+	
+	
+	private boolean isLimitTrialNum = false;
+	private int optionSearchLimit = 100;
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		ignoredTestCaseFiles = new IgnoredTestCaseFiles();
+		parsedTrials = new ParsedTrials();
+		
 		Job job = new Job("Do evaluation") {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				TestCaseEvaluator parser = new TestCaseEvaluator();
+				TestCaseAnalyzer analyzer = new TestCaseAnalyzer();
 				try {
-					parser.runEvaluation(TestCaseEvaluator.TEST_CASE);
+					ExcelReporter reporter = new ExcelReporter(Settings.projectName, unclearRates);
+					String testClassName = "org.apache.commons.math.analysis.interpolation.DividedDifferenceInterpolatorTest";
+					String testMethodName = "testExpm1Function";
+					analyzer.runEvaluationForSingleTestCase(testClassName, testMethodName, reporter, isLimitTrialNum,
+							ignoredTestCaseFiles, parsedTrials, trialNumPerTestCase, unclearRates, optionSearchLimit);
 				} catch (JavaModelException | IOException e) {
 					e.printStackTrace();
 				}
