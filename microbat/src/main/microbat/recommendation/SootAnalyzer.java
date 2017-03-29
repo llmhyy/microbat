915e558945ebf1fddf8bafc633865e8ebfd61946
Yun Lin <llmhyy@gmail.com>
2017-03-29 18:22:46 +0800

    matching hierarchical data structure to soot variable

--- microbat/src/main/microbat/recommendation/SootAnalyzer.java
+++ microbat/src/main/microbat/recommendation/SootAnalyzer.java
@@ -11,8 +11,9 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 
+import microbat.model.BreakPointValue;
+import microbat.model.value.VarValue;
 import microbat.model.variable.FieldVar;
-import microbat.model.variable.LocalVar;
 import microbat.model.variable.Variable;
 import microbat.util.JavaUtil;
 import microbat.util.MicroBatUtil;
@@ -22,11 +23,14 @@ import soot.Local;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
+import soot.Type;
 import soot.Unit;
 import soot.Value;
 import soot.ValueBox;
 import soot.jimple.ArrayRef;
+import soot.jimple.AssignStmt;
 import soot.jimple.FieldRef;
+import soot.jimple.InstanceFieldRef;
 import soot.jimple.internal.JimpleLocal;
 import soot.options.Options;
 import soot.tagkit.LineNumberTag;
@@ -102,12 +106,7 @@ public class SootAnalyzer {
 		SootMethod method = c.getMethod(methodSignature);
 		
 		Body body = method.retrieveActiveBody();
-		Chain<Local> chain = body.getLocals();
-		for(Local local: chain){
-			System.currentTimeMillis();
-		}
-		
-		Variable var = variableOption.getReadVar().getVariable();
+		VarValue var = variableOption.getReadVar();
 		
 		UnitGraph graph = new ExceptionalUnitGraph(body);
 		List<Unit> units = retrieveUnitsAccordingToLineNumber(graph, line);
@@ -119,7 +118,7 @@ public class SootAnalyzer {
 //		List<Unit> dominators = dominatorFinder.getDominators(unit);
 	}
 
-	private List<Unit> findSeeds(Variable var, UnitGraph graph, List<Unit> units) {
+	private List<Unit> findSeeds(VarValue var, UnitGraph graph, List<Unit> units) {
 		HashSet<Unit> parsedUnits = new HashSet<>();
 		List<Unit> allSeeds = new ArrayList<>();
 		for(Unit unit: units){
@@ -138,44 +137,260 @@ public class SootAnalyzer {
 		return allSeeds;
 	}
 
-	private void findDefinitions(Variable var, Unit unit, List<Unit> seeds, UnitGraph graph, HashSet<Unit> parsedUnits) {
+	/**
+	 * reverse traverse the CFG, if a CFG node defines the {@code varValue}, then the algorithm
+	 * stops propagating. Otherwise, the algorithm traverse back to the entry CFG node to find the
+	 * possible CFG node (i.e., unit) defining the {@code varValue}.
+	 * 
+	 * @param varValue
+	 * @param unit
+	 * @param seeds
+	 * @param graph
+	 * @param parsedUnits
+	 */
+	private void findDefinitions(VarValue varValue, Unit unit, List<Unit> seeds, 
+			UnitGraph graph, HashSet<Unit> parsedUnits) {
 		parsedUnits.add(unit);
-		for(ValueBox valueBox: unit.getDefBoxes()){
-			Value val = valueBox.getValue();
-			
-			if((val instanceof JimpleLocal) && (var instanceof LocalVar)){
-				JimpleLocal local = (JimpleLocal)val;
-				if(local.getName().equals(var.getName())){
-					if(!seeds.contains(unit)){
-						seeds.add(unit);					
-					}
-					return;
+
+//		List<VarValue> parentChain = retrieveParentChain(varValue);
+		
+		boolean definingVar = checkUnitDefiningInterestingVariable(unit, graph, seeds, varValue);
+		if(!definingVar){
+			List<Unit> predessors = graph.getPredsOf(unit);
+			for(Unit precessor: predessors){
+				if(!parsedUnits.contains(precessor)){
+					findDefinitions(varValue, precessor, seeds, graph, parsedUnits);				
 				}
-			}
-			
-			if((val instanceof FieldRef) && (var instanceof FieldVar)){
-				FieldRef fieldRef = (FieldRef)val;
-				if(fieldRef.getField().getName().equals(var.getName())){
-					if(!seeds.contains(unit)){
-						seeds.add(unit);					
-					}
-					return;
-				}
-			}
-			
-			if(val instanceof ArrayRef){
-				ArrayRef arrayRef = (ArrayRef)val;
-				//TODO
-				System.currentTimeMillis();
 			}
 		}
 		
-		List<Unit> predessors = graph.getPredsOf(unit);
-		for(Unit precessor: predessors){
-			if(!parsedUnits.contains(precessor)){
-				findDefinitions(var, precessor, seeds, graph, parsedUnits);				
+	}
+
+	/**
+	 * Check whether the unit defines the {@code varValue}. Note that, if a variable is an array
+	 * element or a field of an object instance, e.g., a.k[b].c, we need to recursively decide whether
+	 * the variable corresponding to this static soot variable in CFG node (unit).
+	 * 
+	 * @param unit
+	 * @param graph
+	 * @param seeds
+	 * @param varValue
+	 * @return
+	 */
+	private boolean checkUnitDefiningInterestingVariable(Unit unit, UnitGraph graph, List<Unit> seeds, 
+			VarValue varValue) {
+		
+		for(ValueBox valueBox: unit.getDefBoxes()){
+			Value val = valueBox.getValue();
+			
+			if(isTypeMatch(val, varValue)){
+				/**
+				 * every varValue has a parent node of type BreakPointValue
+				 */
+				if(!(varValue.getParents().get(0) instanceof BreakPointValue)){
+					boolean isRecursiveMatch = resursiveMatch(val, unit, varValue, graph);
+					if(isRecursiveMatch){
+						if(!seeds.contains(unit)){
+							seeds.add(unit);					
+						}
+						return true;
+					}
+				}
+				else{
+					boolean isMatch = match(val, varValue);
+					if(isMatch){
+						if(!seeds.contains(unit)){
+							seeds.add(unit);					
+						}
+						return true;
+					}
+				}
+			}
+			
+		}
+		
+		return false;
+	}
+
+	private boolean isTypeMatch(Value val, VarValue varValue) {
+		Type type = val.getType();
+		
+		if(type.toString().equals(varValue.getVariable().getType())){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * This method is written with regard to the assumption that Soot performs the ``linearization''
+	 * which decompose an expression into a series of three-address instructions. It means if there 
+	 * is a very complicated expression like ``a.k[b].c'', Soot must decompose it into a series of
+	 * simple variable definitions.
+	 * 
+	 * Therefore, we start from {@code definingUnit} which defines the interested variable {@code varValue}, 
+	 * and go back in CFG to search the instructions of linearization. Thus, we can recursively match
+	 * the parents of interested variable to these linearized instruction to see whether two chains of
+	 * variables are matched. For example, obj.array[1] shall be matched to 
+	 * "$r0[1]; $r0 = obj.<sort.quick.Obj: int[] array>; obj = $r2;".
+	 *  
+	 * @param val
+	 * @param definingUnit
+	 * @param varValue
+	 * @param graph
+	 * @return
+	 */
+	private boolean resursiveMatch(Value val, Unit definingUnit, VarValue varValue, UnitGraph graph) {
+		VarValue parent = varValue.getParents().get(0);
+		System.currentTimeMillis();
+		while(parent != null){
+			Value parentVal = findParent(val, definingUnit, graph);
+			if(parentVal==null || !match(parentVal, parent)){
+				return false;
+			}
+			else{
+				if(parent.getParents().isEmpty()){
+					parent = null;
+				}
+				else{
+					parent = parent.getParents().get(0);	
+					if(parent instanceof BreakPointValue){
+						parent = null;
+					}
+					val = parentVal;
+				}
 			}
 		}
+		
+		return true;
+	}
+
+	private Value findParent(Value val, Unit unit, UnitGraph graph) {
+		Unit predessor = null;
+		
+		List<Unit> predessors = graph.getPredsOf(unit);
+		if(!predessors.isEmpty()){
+			predessor = predessors.get(0);			
+		}
+		
+		while(predessor!=null){
+			if(val instanceof ArrayRef){
+				ArrayRef arrayRef = (ArrayRef)val;
+				Value parent = arrayRef.getBase();
+				if(isDefined(parent, predessor)){
+					if(predessor instanceof AssignStmt){
+						AssignStmt assign = (AssignStmt)predessor;
+						Value rightOp = assign.getRightOp();
+						return rightOp;
+						
+//						if(rightOp instanceof FieldRef){
+//							return rightOp;
+//						}
+					}
+				}
+				
+			}
+			else if(val instanceof InstanceFieldRef){
+				InstanceFieldRef insFieldRef = (InstanceFieldRef)val;
+				Value parent = insFieldRef.getBase();
+				if(isDefined(parent, predessor)){
+					return parent;
+				}
+			}
+			else if(val instanceof Local){
+				Local local = (Local)val;
+				//TODO
+				System.currentTimeMillis();
+			}
+			
+			if(!graph.getPredsOf(predessor).isEmpty()){
+				predessor = graph.getPredsOf(predessor).get(0);			
+			}
+			else{
+				predessor = null;
+			}
+		}
+		
+		return null;
+	}
+
+	private boolean isDefined(Value parent, Unit unit) {
+		for(ValueBox box: unit.getDefBoxes()){
+			if(box.getValue().equals(parent)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Unit findNearestDefinition(Value parent, Unit predessor) {
+		
+		return null;
+	}
+
+	private boolean match(Value val, VarValue varValue) {
+		Variable var = varValue.getVariable();
+		if((val instanceof JimpleLocal)){
+			JimpleLocal local = (JimpleLocal)val;
+			if(local.getName().equals(var.getName())){
+				return true;
+			}
+		}
+		
+		if((val instanceof FieldRef) && (var instanceof FieldVar)){
+			FieldRef fieldRef = (FieldRef)val;
+			if(fieldRef.getField().getName().equals(var.getName())){
+				return true;
+			}
+		}
+		
+		if(val instanceof ArrayRef){
+			ArrayRef arrayRef = (ArrayRef)val;
+			//TODO
+			System.currentTimeMillis();
+		}
+		return false;
+	}
+
+	private Local findDefinedVar(Local parent, Unit unitDefiningParent, VarValue child, List<VarValue> parentChain, int i) {
+		if(i<0){
+			
+		}
+		
+		
+		if(child.isElementOfArray()){
+//			Unit accessingUnit = findUnitAccessingArrayElementOfParent();
+//			ValueBox valBox = accessingUnit.getDefBoxes().get(0);
+//			Local local = (Local)valBox.getValue();
+			
+		}
+		else if(child.isField()){
+			
+		}
+		
+		return null;
+	}
+
+	private List<VarValue> retrieveParentChain(VarValue varValue) {
+		List<VarValue> parentChain = new ArrayList<>();
+		VarValue parent = null;
+		if(!varValue.getParents().isEmpty()){
+			parent = varValue.getParents().get(0);
+		}
+		
+		while(parent!=null){
+			if(!parentChain.contains(parent) && !(parent instanceof BreakPointValue)){
+				parentChain.add(parent);
+				if(!parent.getParents().isEmpty()){
+					parent = parent.getParents().get(0);
+				}				
+			}
+			else{
+				break;
+			}
+		}
+		
+		return parentChain;
 	}
 
 	private List<Unit> retrieveUnitsAccordingToLineNumber(UnitGraph graph, int line) {
