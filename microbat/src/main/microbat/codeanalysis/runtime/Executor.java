e8ad9ece88e665b3526cee8f0afa62bb07756d89
Yun Lin <llmhyy@gmail.com>
2017-07-16 17:34:08 +0800

    adapt the jdk1.8 on linux, the "jdk.*" classes should be excluded.

--- microbat/src/main/microbat/codeanalysis/runtime/Executor.java
+++ microbat/src/main/microbat/codeanalysis/runtime/Executor.java
@@ -12,5 +12,6 @@ public abstract class Executor {
 	public static final int TIME_OUT = 10000;
 	
 	protected String[] stepWatchExcludes = { "java.*", "java.lang.*", "javax.*", "sun.*", "com.sun.*", 
-			"org.junit.*", "junit.*", "junit.framework.*", "org.hamcrest.*", "org.hamcrest.core.*", "org.hamcrest.internal.*"};
+			"org.junit.*", "junit.*", "junit.framework.*", "org.hamcrest.*", "org.hamcrest.core.*", "org.hamcrest.internal.*",
+			"jdk.*", "jdk.internal.*"};
 }
