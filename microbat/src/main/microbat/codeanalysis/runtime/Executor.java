564f994ef3c8370b2bd5e3781fd429b4f3c434aa
Yun Lin <llmhyy@gmail.com>
2017-08-06 15:45:12 +0800

    exclude collecting the statements in 3rd party library

--- microbat/src/main/microbat/codeanalysis/runtime/Executor.java
+++ microbat/src/main/microbat/codeanalysis/runtime/Executor.java
@@ -1,5 +1,9 @@
 package microbat.codeanalysis.runtime;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * This class is used to locate all the executor classes in this project
  * @author "linyun"
@@ -14,4 +18,20 @@ public abstract class Executor {
 	protected String[] stepWatchExcludes = { "java.*", "java.lang.*", "javax.*", "sun.*", "com.sun.*", 
 			"org.junit.*", "junit.*", "junit.framework.*", "org.hamcrest.*", "org.hamcrest.core.*", "org.hamcrest.internal.*",
 			"jdk.*", "jdk.internal.*", "org.GNOME.Accessibility.*"};
+	
+	public void addExcludeList(List<String> excludeList) {
+		List<String> existingList = new ArrayList<>();
+		for (int i = 0; i < stepWatchExcludes.length; i++) {
+			existingList.add(stepWatchExcludes[i]);
+		}
+		
+		for (Iterator<String> iterator = excludeList.iterator(); iterator.hasNext();) {
+			String excludeString = (String) iterator.next();
+			if (!existingList.contains(excludeString)) {
+				existingList.add(excludeString);
+			}
+		}
+		
+		stepWatchExcludes = existingList.toArray(new String[0]);
+	}
 }
