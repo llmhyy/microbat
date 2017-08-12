522c7a33e9bd69b0b1fb9c23115a2d11f19fec66
Yun Lin <llmhyy@gmail.com>
2017-08-12 21:09:38 +0800

    optimize when retrieving a complicated method invocation expression

--- microbat/src/main/microbat/codeanalysis/runtime/Executor.java
+++ microbat/src/main/microbat/codeanalysis/runtime/Executor.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import com.sun.jdi.ClassNotLoadedException;
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.InvalidTypeException;
+import com.sun.jdi.InvocationException;
 import com.sun.jdi.ThreadReference;
+import com.sun.jdi.Value;
+import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.request.BreakpointRequest;
 import com.sun.jdi.request.ClassPrepareRequest;
 import com.sun.jdi.request.EventRequest;
@@ -14,6 +20,10 @@ import com.sun.jdi.request.MethodEntryRequest;
 import com.sun.jdi.request.MethodExitRequest;
 import com.sun.jdi.request.StepRequest;
 import com.sun.jdi.request.ThreadStartRequest;
+
+import microbat.codeanalysis.runtime.jpda.expr.ExpressionParser;
+import microbat.codeanalysis.runtime.jpda.expr.ExpressionParser.GetFrame;
+import microbat.codeanalysis.runtime.jpda.expr.ParseException;
 
 /**
  * This class is used to locate all the executor classes in this project
@@ -83,6 +93,11 @@ public abstract class Executor {
 		this.stepRequestList.add(stepRequest);
 	}
 	
+	protected boolean hasValidThreadName(ThreadReference thread) {
+		return !thread.name().equals("main") && !thread.name().equals("DestroyJavaVM")
+				&& !thread.name().startsWith("Thread");
+	}
+	
 	protected void addThreadStartWatch(EventRequestManager erm) {
 		threadStartRequest = erm.createThreadStartRequest();
 		threadStartRequest.setSuspendPolicy(EventRequest.SUSPEND_EVENT_THREAD);
@@ -117,18 +132,65 @@ public abstract class Executor {
 			exceptionRequest.addClassExclusionFilter(junitString);
 		}
 		
+		methodEntryRequest.setEnabled(methodEntrySwtich);
+		methodExitRequest.setEnabled(methodExistSwtich);
+		classPrepareRequest.setEnabled(classPrepareSwtich);
+		exceptionRequest.setEnabled(exceptionSwtich);
 		for(int i=0; i<stepRequestList.size(); i++) {
 			StepRequest stepRequest = stepRequestList.get(i);
 			try {
 				stepRequest.setEnabled(stepSwitch.get(i));
 			} catch (Exception e) {
 				e.printStackTrace();
-			}
-//			System.currentTimeMillis();
+			}		
 		}
+	}
+	
+	protected Value retrieveComplicatedExpressionValue(String expr, VirtualMachine vm, GetFrame frameGetter) {
+		List<Boolean> stepSwitch = new ArrayList<>();
+		for(int i=0; i<stepRequestList.size(); i++) {
+			StepRequest stepRequest = stepRequestList.get(i);
+			stepSwitch.add(stepRequest.isEnabled());
+			stepRequest.disable();
+		}
+		
+		boolean methodEntrySwtich = methodEntryRequest.isEnabled();
+		methodEntryRequest.disable();
+		boolean methodExistSwtich = methodExitRequest.isEnabled();
+		methodExitRequest.disable();
+		boolean classPrepareSwtich = classPrepareRequest.isEnabled();
+		classPrepareRequest.disable();
+		boolean exceptionSwtich = exceptionRequest.isEnabled();
+		exceptionRequest.disable();
+		
+		Value val = null;
+		try {
+			val = ExpressionParser.evaluate(expr, vm, frameGetter);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		} catch (InvocationException e1) {
+			e1.printStackTrace();
+		} catch (InvalidTypeException e1) {
+			e1.printStackTrace();
+		} catch (ClassNotLoadedException e1) {
+			e1.printStackTrace();
+		} catch (IncompatibleThreadStateException e1) {
+			e1.printStackTrace();
+		} 
+		
 		methodEntryRequest.setEnabled(methodEntrySwtich);
 		methodExitRequest.setEnabled(methodExistSwtich);
 		classPrepareRequest.setEnabled(classPrepareSwtich);
 		exceptionRequest.setEnabled(exceptionSwtich);
+		for(int i=0; i<stepRequestList.size(); i++) {
+			StepRequest stepRequest = stepRequestList.get(i);
+			try {
+				stepRequest.setEnabled(stepSwitch.get(i));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}		
+		}
+		
+		return val;
 	}
 }
