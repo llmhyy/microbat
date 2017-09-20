da9a18f1387da6f49d637a28ddd02c3a43fe00d2
Yun Lin <llmhyy@gmail.com>
2017-09-19 23:33:37 -0700

    clean

--- microbat/src/main/microbat/handler/UndoHandler.java
+++ microbat/src/main/microbat/handler/UndoHandler.java
@@ -4,7 +4,6 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 import microbat.behavior.Behavior;
 import microbat.behavior.BehaviorData;
