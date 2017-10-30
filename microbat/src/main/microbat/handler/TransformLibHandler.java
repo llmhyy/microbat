b9abc8c4b36ec08109c786616da972b0a1f6112a
Yun Lin <llmhyy@gmail.com>
2017-10-31 03:59:29 +0800

    create a transfer menu on microbat UI

---
+++ microbat/src/main/microbat/handler/TransformLibHandler.java
@@ -0,0 +1,17 @@
+package microbat.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class TransformLibHandler extends AbstractHandler {
+
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("do");
+		return null;
+	}
+
+	
+}
