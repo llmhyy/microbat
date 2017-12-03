5ee2bdc904b73a370eee1d7c61b7fe5c83b6e20e
Yun Lin <llmhyy@gmail.com>
2017-12-03 21:00:41 +0800

    revise database schema

---
+++ microbat/ddl/Regression.sql
@@ -0,0 +1,19 @@
+USE trace
+;
+DROP TABLE IF EXISTS Regression
+;
+CREATE TABLE Regression
+(
+	regression_id INTEGER NOT NULL AUTO_INCREMENT,
+	buggy_trace INTEGER,
+	correct_trace INTEGER,
+	root_cause_step INTEGER,
+	is_overskip INTEGER,
+	over_skip_number INTEGER,
+	control_mending_start INTEGER,
+	data_mending_start INTEGER,
+	PRIMARY KEY (regression_id)
+) 
+;
+
+
