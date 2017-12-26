4ead024482f9932a8e2939d601cc2269ba0f5f1f
Tran Thi Ly Ly <lylypp6987@gmail.com>
2017-12-26 12:29:59 +0800

    implement deriveLibExcludePatterns

--- microbat/src/main/microbat/codeanalysis/runtime/Executor.java
+++ microbat/src/main/microbat/codeanalysis/runtime/Executor.java
@@ -1,8 +1,18 @@
 package microbat.codeanalysis.runtime;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.PackageFragment;
 
 import com.sun.jdi.ClassNotLoadedException;
 import com.sun.jdi.IncompatibleThreadStateException;
@@ -25,6 +35,10 @@ import com.sun.jdi.request.ThreadStartRequest;
 import microbat.codeanalysis.runtime.jpda.expr.ExpressionParser;
 import microbat.codeanalysis.runtime.jpda.expr.ExpressionParser.GetFrame;
 import microbat.codeanalysis.runtime.jpda.expr.ParseException;
+import microbat.util.FilterUtils;
+import microbat.util.JavaUtil;
+import sav.common.core.utils.CollectionUtils;
+import sav.common.core.utils.StringUtils;
 
 /**
  * This class is used to locate all the executor classes in this project
@@ -96,11 +110,103 @@ public abstract class Executor {
 	 *
 	 * @return
 	 */
-	public String[] deriveLibExcludePatterns() {
-		//TODO
-		return null;
+	public static String[] deriveLibExcludePatterns() {
+		Set<String> newExcludeCol = CollectionUtils.toHashSet(libExcludes);
+		IJavaProject ijavaProject = JavaCore.create(JavaUtil.getSpecificJavaProjectInWorkspace());
+		for (String incl : libIncludes) {
+			Set<String> expandedExclSet = new HashSet<String>();
+			for (Iterator<String> it = newExcludeCol.iterator(); it.hasNext();) {
+				String excl = it.next();
+				if (excl.equals(incl)) {
+					it.remove();
+					break;
+				}
+				if (FilterUtils.isSubFilter(incl, excl)) {
+					it.remove();
+					expandPkgFilter(ijavaProject, excl, incl, expandedExclSet);
+				}
+			}
+			newExcludeCol.addAll(expandedExclSet);
+		}
+		return StringUtils.sortAlphanumericStrings(new ArrayList<>(newExcludeCol)).toArray(new String[0]);
 	}
 	
+	/**
+	 *  java.util.* : include all types and packages under java.util package
+	 *  java.util.*\ : include all types only under java.util package
+	 *  java.util.Arrays : include type Arrays only
+	 *  java.util.Arrays\ : include type Arrays and its inner types
+	 */
+	private static void expandPkgFilter(IJavaProject javaProject, String pkgFilter, String incl,
+			Set<String> expandedExclSet) {
+		try {
+			List<IPackageFragment> matches = new ArrayList<IPackageFragment>();
+			String pkgName = FilterUtils.getPrefix(pkgFilter);
+			/* find packages match pkgFilter */
+			for (IPackageFragmentRoot pkgRoot : javaProject.getPackageFragmentRoots()) {
+				IPackageFragment pkg = pkgRoot.getPackageFragment(pkgName);
+				if (pkg.exists()) {
+					matches.add(pkg);
+				}
+			}
+			/* get include package */
+			String[] inclFrags;
+			String inclTypeSimpleName = null;
+			String subTypePrefix = null;
+			int typeSimpleNameSIdx = incl.lastIndexOf(".");
+			inclFrags = StringUtils.dotSplit(incl.substring(0, typeSimpleNameSIdx));
+			if (incl.endsWith("*\\")) {
+				inclTypeSimpleName = "";
+			} else if (!incl.endsWith("*")) {
+				if (!incl.endsWith("\\")) {
+					inclTypeSimpleName = incl.substring(typeSimpleNameSIdx + 1);
+					subTypePrefix = inclTypeSimpleName + "$";
+				} else {
+					inclTypeSimpleName = incl.substring(typeSimpleNameSIdx + 1, incl.length() - 1);
+				}
+			} 
+			/* build up exclude list */
+			for (IPackageFragment pkg : matches) {
+				/* add exclude packages */
+				for (IJavaElement otherPkg : ((IPackageFragmentRoot) pkg.getParent()).getChildren()) {
+					String[] otherPkgFrags = ((PackageFragment) otherPkg).names;
+					int i = 0;
+					for (i = 0; i < inclFrags.length && i < otherPkgFrags.length; i++) {
+						if (!inclFrags[i].equals(otherPkgFrags[i])) {
+							break;
+						}
+					}
+					if (i == otherPkgFrags.length) {
+						/* ignore include package's parent */
+						continue;
+					}
+					if (i < inclFrags.length) {
+						expandedExclSet.add(FilterUtils.toPkgFilterText(otherPkgFrags, 0, i));
+					} else if (i == inclFrags.length && i < otherPkgFrags.length  && inclTypeSimpleName != null) {
+						/* add sub-package of include package */
+						expandedExclSet.add(FilterUtils.toPkgFilterText(otherPkgFrags, 0, i));
+					} 
+				}
+				/* add exclude types */
+				if (inclTypeSimpleName != null) {
+					for (IJavaElement child : pkg.getChildren()) {
+						if (child.getElementType() == IJavaElement.CLASS_FILE) {
+							String typeSimpleName = child.getElementName().replace(".class", "");
+							if (inclTypeSimpleName.isEmpty() || inclTypeSimpleName.equals(typeSimpleName)
+									|| (subTypePrefix != null && typeSimpleName.startsWith(subTypePrefix))) {
+								continue;
+							}
+							expandedExclSet.add(StringUtils.dotJoin(pkgName, typeSimpleName));
+						}
+					}
+				}
+			}
+		} catch (JavaModelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
 	protected boolean isInIncludedLibrary(Location currentLocation) {
 		String typeName = currentLocation.declaringType().name();
 		for(String expr: libIncludes){
@@ -160,7 +266,6 @@ public abstract class Executor {
 			classPrepareRequest.addClassExclusionFilter(ex);
 		}
 		classPrepareRequest.setEnabled(true);
-
 	}
 	
 	/**
