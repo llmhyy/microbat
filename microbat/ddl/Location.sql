5d189f17143e8b81992225b64426e1156e99e920
Yun Lin <llmhyy@gmail.com>
2018-01-09 14:57:38 +0800

    change the type of location_id in db schema into integer

--- microbat/ddl/Location.sql
+++ microbat/ddl/Location.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS Location
 ;
 CREATE TABLE Location
 (
-	location_id VARCHAR(255) NOT NULL,
+	location_id INTEGER NOT NULL AUTO_INCREMENT,
 	trace_id INTEGER NOT NULL,
 	class_name VARCHAR(255),
 	line_number INTEGER,
