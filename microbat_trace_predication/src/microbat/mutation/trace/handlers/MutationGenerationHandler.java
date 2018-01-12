f78a7bc46242f9bdfb154b4f741072176df122b2
Yun Lin <llmhyy@gmail.com>
2018-01-12 16:12:50 +0800

    init for trace predication project

---
+++ microbat_trace_predication/src/microbat/mutation/trace/handlers/MutationGenerationHandler.java
@@ -0,0 +1,26 @@
+package microbat.mutation.trace.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class MutationGenerationHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Microbat_mutation_trace",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
