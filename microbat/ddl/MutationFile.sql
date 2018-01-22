5bb8fc9c1f70f1344075ef6ceb057af01707a79b
Yun Lin <llmhyy@gmail.com>
2018-01-22 15:36:20 +0800

    update Mutation table

--- microbat/ddl/MutationFile.sql
+++ microbat/ddl/MutationFile.sql
@@ -1,7 +1,12 @@
+USE trace
+;
+DROP TABLE IF EXISTS MutationFile
+;
 CREATE TABLE MutationFile
 (
 	trace_id INTEGER,
-	mutationFile BLOB
+	mutation_file BLOB,
+	mutation_class_name VARCHAR(255)
 ) 
 ;
 
