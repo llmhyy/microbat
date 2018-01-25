071eef6935a16be4d8354375daf444da4a45a502
Yun Lin <llmhyy@gmail.com>
2018-01-25 20:40:24 +0800

    update db schema

--- microbat/ddl/DeadEndRecord.sql
+++ microbat/ddl/DeadEndRecord.sql
@@ -7,7 +7,7 @@ CREATE TABLE DeadEndRecord
 	regression_id INTEGER,
 	record_type INTEGER,
 	occur_order INTEGER,
-	correspondence_order INTEGER,
+	dead_end_order INTEGER,
 	break_step_order INTEGER,
 	variable TEXT
 ) 
