04c6d6077ed277cab4138b236cd52d37146569d5
Yun Lin <llmhyy@gmail.com>
2018-03-14 02:21:48 -0700

    record the instructions in specific line

--- microbat/src/main/microbat/codeanalysis/bytecode/MethodFinderByLine.java
+++ microbat/src/main/microbat/codeanalysis/bytecode/MethodFinderByLine.java
@@ -1,12 +1,19 @@
 package microbat.codeanalysis.bytecode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.bcel.classfile.LineNumber;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.InstructionList;
 
 import microbat.model.BreakPoint;
 
 public class MethodFinderByLine extends ByteCodeMethodFinder {
 	private BreakPoint point;
+	private List<InstructionHandle> handles = new ArrayList<>();
+	
 	
 	public BreakPoint getPoint() {
 		return point;
@@ -22,11 +29,23 @@ public class MethodFinderByLine extends ByteCodeMethodFinder {
 	
 	public void visitMethod(Method method){
 		if(method.getLineNumberTable()!=null){
+			
 			for(LineNumber lineNumber: method.getLineNumberTable().getLineNumberTable()){
 				if(lineNumber.getLineNumber()==point.getLineNumber()){
 					this.setMethod(method);
 				}
-			}			
+			}
+			
+			if(method.getCode()!=null){
+				InstructionList insList = new InstructionList(method.getCode().getCode());
+				for(InstructionHandle handle: insList){
+					int line = method.getLineNumberTable().getSourceLine(handle.getPosition());
+					if(line == point.getLineNumber()){
+						this.handles.add(handle);
+					}
+				}
+				
+			}
 		}
 	}
 }
