7587cff5e39c26350f10717250419e5ae2d6f876
Yun Lin <llmhyy@gmail.com>
2018-03-14 22:16:37 +0800

    get a bytecode class from ByteCodeParser

--- microbat/src/main/microbat/codeanalysis/bytecode/ByteCodeParser.java
+++ microbat/src/main/microbat/codeanalysis/bytecode/ByteCodeParser.java
@@ -12,6 +12,45 @@ import org.apache.bcel.util.SyntheticRepository;
 import sav.strategies.dto.AppJavaClassPath;
 
 public class ByteCodeParser{
+	public static JavaClass parse(String className, AppJavaClassPath appClassPath) {
+		String originalSystemClassPath = System.getProperty("java.class.path");
+		String[] paths = originalSystemClassPath.split(File.pathSeparator);
+		
+		try{
+			List<String> pathList = new ArrayList<>();
+			for(String path: paths){
+				pathList.add(path);
+			}
+			
+			StringBuffer buffer = new StringBuffer(originalSystemClassPath);
+			for(String classPath: appClassPath.getClasspaths()){
+				if(!pathList.contains(classPath)){
+					buffer.append(File.pathSeparator + classPath);				
+				}
+			}
+			buffer.append(File.pathSeparator);
+			String jdkPath = appClassPath.getJavaHome() + File.separator + "jre" + 
+					File.separator + "lib" + File.separator + "rt.jar";
+			buffer.append(jdkPath);
+			System.setProperty("java.class.path", buffer.toString());
+			String s = System.getProperty("java.class.path");
+			
+			ClassPath0 classPath = new ClassPath0(s);
+			Repository.setRepository(SyntheticRepository.getInstance(classPath));
+			
+			JavaClass clazz = Repository.lookupClass(className);
+			return clazz;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			System.setProperty("java.class.path", originalSystemClassPath);			
+		}
+		
+		return null;
+	}
+	
 	public static void parse(String className, ByteCodeVisitor visitor, AppJavaClassPath appClassPath){
 		String originalSystemClassPath = System.getProperty("java.class.path");
 		String[] paths = originalSystemClassPath.split(File.pathSeparator);
