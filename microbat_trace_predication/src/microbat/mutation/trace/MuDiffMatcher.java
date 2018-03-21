4b6468151668c628b10575039de0d4c352c9fc72
lylytran <lylypp6987@gmail.com>
2018-03-21 06:59:25 -0700

    override the way of getting DeclarationCompilationUnit for diff java file

--- microbat_trace_predication/src/microbat/mutation/trace/MuDiffMatcher.java
+++ microbat_trace_predication/src/microbat/mutation/trace/MuDiffMatcher.java
@@ -1,8 +1,10 @@
 package microbat.mutation.trace;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import tregression.separatesnapshots.DiffMatcher;
+import tregression.separatesnapshots.diff.FilePairWithDiff;
 
 public class MuDiffMatcher extends DiffMatcher {
 	private String buggySourcePath;
@@ -12,6 +14,29 @@ public class MuDiffMatcher extends DiffMatcher {
 		super(sourceFolder, null, null, null);
 		this.buggySourcePath = mutationFilePath;
 		this.fixSourcePath = orgFilePath;
+	}
+	
+	@Override
+	public void matchCode() {
+		super.matchCode();
+		List<FilePairWithDiff> orgFileDiffList = this.fileDiffList;
+		List<FilePairWithDiff> wrapFileDiffList = new ArrayList<>();
+		for (FilePairWithDiff diff : orgFileDiffList) {
+			FilePairWithDiff wrapDiff = new FilePairWithDiff() {
+				@Override
+				public String getSourceDeclaringCompilationUnit() {
+					return getTargetDeclaringCompilationUnit();
+				}
+			};
+			wrapDiff.setSourceToTargetMap(diff.getSourceToTargetMap());
+			wrapDiff.setTargetToSourceMap(diff.getTargetToSourceMap());
+			wrapDiff.setSourceFile(diff.getSourceFile());
+			wrapDiff.setTargetFile(diff.getTargetFile());
+			wrapDiff.setSourceFolderName(diff.getSourceFolderName());
+			wrapDiff.setChunks(diff.getChunks());
+			wrapFileDiffList.add(wrapDiff);
+		}
+		this.fileDiffList = wrapFileDiffList;
 	}
 	
 	protected List<String> getRawDiffContent(){
