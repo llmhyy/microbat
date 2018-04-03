3e8658d370a2938613c42783a755f2d9b7cb6ca2
lylytran <lylypp6987@gmail.com>
2018-04-03 14:04:01 +0800

    clean up

--- microbat_instrumentator/src/main/microbat/instrumentation/ClassGenUtils.java
+++ microbat_instrumentator/src/main/microbat/instrumentation/ClassGenUtils.java
@@ -8,17 +8,6 @@ public class ClassGenUtils {
 		StringBuilder sb = new StringBuilder();
 		sb.append(className).append("#").append(method.getName())
 			.append(method.getSignature().replace(";", ":"));
-//		.append("(");
-//		Type[] argTypes = method.getArgumentTypes();
-//		int lastIdx = argTypes.length - 1;
-//		for (int i = 0; i < argTypes.length; i++) {
-//			sb.append(argTypes[i].getSignature());
-//			if (i != lastIdx) {
-//				sb.append(";");
-//			}
-//		}
-//		sb.append(")");
-//		sb.append(method.getReturnType().getSignature());
 		return sb.toString();
 	}
 	
