67b67e5c21149bc5a55fe81ec111af85c12efc87
lylytran <lylypp6987@gmail.com>
2018-05-31 12:16:25 +0800

    fix copy-paste: remove redundant code

--- microbat_instrumentator/src/main/microbat/instrumentation/Agent.java
+++ microbat_instrumentator/src/main/microbat/instrumentation/Agent.java
@@ -78,7 +78,6 @@ public class Agent {
 				}
 			}
 		}
-		candidates.remove(Thread.class);
 		AgentLogger.debug(candidates.size() + " retransformable candidates");
 		if (candidates.isEmpty()) {
 			return null;
