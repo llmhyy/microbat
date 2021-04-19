2284b366f98f2f24f475eb9cd198825478b3c26b
dingyuchen <dingyuchen@gmail.com>
2021-04-19 18:00:56 +0800

    Set trace_id as uuid string

--- microbat/ddl/Location.sql
+++ microbat/ddl/Location.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Location ( 
 	location_id integer NOT NULL,
-	trace_id integer NOT NULL,
+	trace_id TEXT NOT NULL,
 	class_name varchar(255),
 	line_number integer,
 	is_conditional integer,
