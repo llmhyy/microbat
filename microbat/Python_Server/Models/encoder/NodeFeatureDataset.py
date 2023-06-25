eee3a888eef3c5dd4c4f23ec135dfb03f39d1642
ykwongaq <ykwongaq@connect.ust.hk>
2023-06-25 15:15:20 +0800

    Update python server and encoder

---
+++ microbat/Python_Server/Models/encoder/NodeFeatureDataset.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+import os
+import json
+
+from torch.utils.data import Dataset
+from joblib import Parallel, delayed
+
+class NodeFeatureDataset(Dataset):
+    def __init__(self, config):
+        self.config = config
+        self.train_data_path = self.config["data.train_data_path"]
+        assert os.path.exists(self.train_data_path), f"Give data path does not exists: {self.train_data_path}"
+        assert self.train_data_path.endswith(".json"), f"GIve data path is not a json file: {self.train_data_path}"
+        self.num_workers = self.config["data.num_workers"]
+        self.cos_sim = nn.CosineSimilarity(dim=-1)
+        self.readData()
+
+
+    def readData(self):
+        print(f"Reading data from {self.train_data_path} ...")
+        self.data = []
+        self.labels = []
+        self.label2data = {}
+        idx = 0
+        with open(self.train_data_path, "r") as file:
+            for line in file:
+                obj = json.loads(line)
+                feature = obj["feature"]
+                label = obj["label"]
+                self.data.append(feature)
+                self.labels.append(label)
+                if label in self.label2data:
+                    self.label2data[label].append(idx)
+                else:
+                    self.label2data[label] = [idx]
+                idx += 1
+        
+        for label, items in self.label2data.items():
+            if len(items) <= 1:
+                raise Exception(f"There are only one element in label {label}, please filter it out from dataset")
+
+        print(f"Collected {len(self.data)} data ...")
+        
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        feature = self.data[index]
+        feature = self.to_tensor(feature)
+        pos_feature = self.get_pos_sample(index)
+        neg_feature = self.get_neg_sample(index)
+        label = self.labels[index]
+        label = self.to_tensor(label)
+        return feature, pos_feature, neg_feature, label
+
+    def get_pos_sample(self, index):
+        label = self.labels[index]
+        positive_idxes = self.label2data[label]
+        positive_idxes.remove(index) # Don't sample itself as positive sample
+        positive_idx = random.sample(positive_idxes, 1)[0]
+        pos_feature = self.data[positive_idx]
+        return self.to_tensor(pos_feature)
+
+
+    def get_neg_sample(self, index):
+        label = self.labels[index]
+        labels_ = list(self.label2data.keys())
+        labels_.remove(label)
+        neg_label = random.sample(labels_, k=1)[0]
+        negative_idxes = self.label2data[neg_label]
+        negative_idx = random.sample(negative_idxes, 1)[0]
+        neg_feature = self.data[negative_idx]
+        return self.to_tensor(neg_feature)
+
+    def to_tensor(self, feature):
+        return torch.tensor(feature)
+    # def __getitem__(self, index) -> torch.Tensor:
+    #     feature = self.data[index]
+        
+    #     anchor_feature, classification_vector, code_statement = self.data[index]
+        
+    #     # Randomly select one postive sample and negative sample
+    #     sampled_idxes = random.sample(range(len(self.data)), k=100)
+    #     if index in sampled_idxes:
+    #         sampled_idxes.remove(index)
+    #     sampled_idxes = torch.tensor(sampled_idxes)
+
+    #     sampled_cls_vector = [self.data[idx][1] for idx in sampled_idxes]
+    #     sampled_cls_vector = torch.stack(sampled_cls_vector)
+    #     # hamming_dis = (classification_vector.bool() ^ sampled_cls_vector.bool()).sum(dim=-1)
+
+    #     cos_sim = F.cosine_similarity(
+    #         F.normalize(sampled_cls_vector, p=2, dim=-1),
+    #         F.normalize(classification_vector, p=2, dim=-1).unsqueeze(dim=0),
+    #         dim=-1)
+        
+    #     pos_idx = cos_sim >= 0.5
+    #     neg_idx = cos_sim < 0.5
+    #     # pos_idx = hamming_dis <= 1
+    #     # neg_idx = hamming_dis > 1
+
+    #     if pos_idx.sum() == 0:
+    #         print("missing positive")
+    #         return torch.zeros_like(anchor_feature), torch.zeros_like(anchor_feature), torch.zeros_like(anchor_feature), torch.Tensor([4])
+        
+    #     if neg_idx.sum() == 0:
+    #         print("missing negative")
+    #         return torch.zeros_like(anchor_feature), torch.zeros_like(anchor_feature), torch.zeros_like(anchor_feature), torch.Tensor([4])
+
+    #     pos_feature = self.data[sampled_idxes[pos_idx][0]][0]
+    #     neg_feature = self.data[sampled_idxes[neg_idx][0]][0]
+
+    #     class_id = self.cls_vector_to_id(classification_vector)
+    #     return anchor_feature, pos_feature, neg_feature, class_id
+    
+    # def cls_vector_to_id(self, cls_vector):
+    #     if (cls_vector == self.class_0).all():
+    #         return torch.Tensor([0])
+    #     elif (cls_vector == self.class_1).all():
+    #         return torch.Tensor([1])
+    #     elif (cls_vector == self.class_2).all():
+    #         return torch.Tensor([2])
+    #     elif (cls_vector == self.class_3).all():
+    #         return torch.Tensor([3])
+    #     else:
+    #         return torch.Tensor([4])
---
+++ microbat/Python_Server/Models/train_encoder.py
@@ -0,0 +1,135 @@
+import yaml
+import os
+
+from encoder.NodeFeatureDataset import NodeFeatureDataset
+from encoder.Encoder import Encoder
+
+from sklearn.manifold import TSNE
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
+import torchvision.transforms as transforms
+
+class Trainer():
+    def __init__(self,config):
+        self.config = config
+        self.num_epoch = self.config["training.num_epoch"]
+        self.lr = self.config["training.lr"]
+        self.device = self.config["training.device"]
+        self.save_interval = self.config["training.save_interval"]
+        self.output_folder = os.path.join(self.config["training.output_path"], self.config["training.version"])
+        if self.device == "cuda":
+            assert torch.cuda.is_available(), "Cuda is not avaliable"
+        
+        self.batch_size = self.config["data.batch_size"]
+        self.num_workers = self.config["data.num_workers"]
+        
+        self.log_interval = self.config["visualization.log_interval"]
+        self.vis_sample_size = self.config["visualization.sample_size"]
+
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+
+        # Training config
+        self.model = Encoder(self.config).to(device=self.device)
+        self.loss_fn = nn.TripletMarginLoss(margin=1.0, p=2).to(device=self.device)
+        self.optimizier = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.scheduler = optim.lr_scheduler.LinearLR(self.optimizier, start_factor=1.0, end_factor=0.001, total_iters=self.num_epoch)
+
+        # Dataloader
+        dataset = NodeFeatureDataset(self.config)
+        self.total_step = dataset.__len__() // self.batch_size
+        self.dataloader = DataLoader(dataset, 
+                                     batch_size=self.batch_size, 
+                                     drop_last=True,
+                                     shuffle=True)
+
+        # Visualization
+        self.writer = SummaryWriter(log_dir=self.output_folder)
+        self.tsne = TSNE(n_components=2)
+
+    def train(self):
+        self.model.train()
+        
+        step = 0
+        for epoch in range(self.num_epoch):
+            vis_features = []
+            vis_class = []
+            step_ = 0
+            for anchor_feature, pos_feature, neg_feature, labels in self.dataloader:
+                self.optimizier.zero_grad()
+                
+                anchor_embedding = self.model(anchor_feature.to(self.device))
+                pos_embedding = self.model(pos_feature.to(self.device))
+                neg_embedding = self.model(neg_feature.to(self.device))
+
+                triplet_loss = self.loss_fn(anchor_embedding, pos_embedding, neg_embedding)
+                
+                if step % self.log_interval == 0:
+                    print(f"[Training log] epoch: {epoch}/{self.num_epoch} step: {step_}/{self.total_step} triplet loss: {triplet_loss} lr: {self.scheduler.get_last_lr()}")
+                    self.writer.add_scalar('Loss', triplet_loss.item(), step)
+
+                if len(vis_features) < self.vis_sample_size:
+                    B = anchor_embedding.shape[0]
+                    indices = torch.randperm(B)[:self.vis_sample_size]
+                    selected_features = anchor_embedding[indices]
+                    selected_features = selected_features.detach().cpu()
+                    selected_class = labels[indices]
+                    vis_features.extend(list(selected_features))
+                    vis_class.extend(list(selected_class))
+                    
+                triplet_loss.backward()
+                self.optimizier.step()
+                step += 1
+                step_ += 1
+            
+            self.scheduler.step()
+
+            # Visualization
+            print("Visualizing ...")
+            vis_features = torch.stack(vis_features)
+            vis_class = torch.stack(vis_class) 
+            vis = self.tsne.fit_transform(vis_features.numpy())
+            plt.clf()
+            plt.scatter(vis[:, 0], vis[:, 1], c=vis_class)
+            image_path = os.path.join(self.output_folder, "temp.png")
+            plt.savefig(image_path)
+            image_rgba = plt.imread(image_path)
+            image_rgb = Image.fromarray((image_rgba * 255).astype(np.uint8)).convert('RGB')
+            transform = transforms.ToTensor()
+            image_tensor = transform(image_rgb)
+            self.writer.add_image("projected embedding", image_tensor, global_step=epoch)
+
+            # Save model
+            if epoch+1 % self.save_interval == 0:
+                self.save_model(epoch+1)
+            
+
+    def save_model(self, epoch):
+        output_path = os.path.join(self.output_folder, f"epoch_{epoch}.pt")
+        print(f"Saving model to {output_path}")
+        torch.save(self.model.state_dict(), output_path)
+
+def main(config):
+    trainer = Trainer(config=config)
+    trainer.train()
+
+if __name__ == '__main__':
+    file_path = "C:\\Users\\david\\Documents\\Python\\Models\\configs\\encoder_config.yaml"
+
+    # Open the YAML file
+    with open(file_path, "r") as yaml_file:
+        # Load the contents of the YAML file
+        config = yaml.safe_load(yaml_file)
+
+    print("Configuration ------------------")
+    for key, value in config.items():
+        print(key, value)
+    print("-" * 20)
+    main(config)
