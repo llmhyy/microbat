e9aeded9ea208c159c969869b324546dfdb41268
ww051028 <ykwongaq@connect.ust.hk>
2023-08-05 23:50:39 +0800

    Remove comment

--- microbat/src/main/microbat/debugpilot/propagation/spp/SPPCF.java
+++ microbat/src/main/microbat/debugpilot/propagation/spp/SPPCF.java
@@ -19,25 +19,6 @@ public class SPPCF extends SPPH {
 		super(settings);
 	}
 
-//	@Override
-//	protected void backwardProp() {
-//		try {
-//			this.client.conntectServer();
-//			this.client.sendFeedbackVectors(this.feedbackRecords);
-//			super.backwardProp();
-//			this.client.notifyStop();
-//			this.client.disconnectServer();
-//		} catch (IOException | InterruptedException e) {
-//			throw new RuntimeException(Log.genMsg(getClass(), "Server connection problem"));
-//		}
-//	}
-//	
-//	@Override
-//	protected double calForwardFactor(TraceNode node) {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
-
 	@Override
 	protected double calBackwardFactor(VarValue var, TraceNode node) {
 		if (this.feedbackRecords.isEmpty()) {
@@ -95,73 +76,6 @@ public class SPPCF extends SPPH {
 		}
 		return wrongVar;
 	}
-	
-	
-//	@Override
-//	protected void calConditionBackwardFactor(final TraceNode node) {
-//		final TraceNode controlDom = node.getControlDominator();
-//		if (controlDom != null) {
-//			if (this.feedbackRecords.isEmpty()) {
-//				super.calConditionBackwardFactor(node);
-//			} else {
-//				final VarValue conditionResult = controlDom.getConditionResult();
-//				List<ModelPrediction> predictions = this.askForPredictions(node, conditionResult);
-//				this.storeReason(node, conditionResult, predictions);
-//				final boolean isAllUncertain = predictions.stream().allMatch(prediction -> prediction.equals(ModelPrediction.UNCERTAIN));
-//				if (isAllUncertain) {
-//					super.calConditionBackwardFactor(node);
-//				} else {
-//					
-//				}
-//			}
-//		}
-//	}
-	
-//	protected double calFactor(final TraceNode node, final VarValue var) {
-//		try {
-//			this.client.notifyContinuoue();
-//			this.client.sendContextFeature(node);
-//			this.client.sendVariableVector(var);
-//			this.client.sendVariableName(var);
-//			final String response = this.client.recieveModelPredictionString();
-//			List<ModelPrediction> predictions = ModelPrediction.parseStringList(response);
-//			this.storeReason(node, var, predictions);
-//			final boolean isAllUncertain = predictions.stream().allMatch(prediction -> prediction.equals(ModelPrediction.UNCERTAIN));
-//			if (isAllUncertain) {
-//				return super.calBackwardFactor(var, node);
-//			} else {
-//				return predictions.stream().filter(prediction -> !prediction.equals(ModelPrediction.UNCERTAIN))
-//						.mapToDouble(prediction -> prediction.equals(ModelPrediction.SUSPICION) ? 1.0d : 0.0d)
-//						.average().orElse(-1.0d);
-//			}
-//		} catch (IOException | InterruptedException e) {
-//			throw new RuntimeException(Log.genMsg(getClass(), "Server connection problem"));
-//		}
-//	}
-	
-//	protected double calFactor(final VarValue var, final TraceNode node, List<ModelPrediction> predictions) {
-//		final boolean isAllUncertain = predictions.stream().allMatch(prediction -> prediction.equals(ModelPrediction.UNCERTAIN));
-//		if (isAllUncertain) {
-//			return super.calBackwardFactor(var, node);
-//		} else {
-//			return predictions.stream().filter(prediction -> !prediction.equals(ModelPrediction.UNCERTAIN))
-//					.mapToDouble(prediction -> prediction.equals(ModelPrediction.SUSPICION) ? 1.0d : 0.0d)
-//					.average().orElse(-1.0d);
-//		}
-//	}
-//	
-//	protected double calHeuristicFactor(final TraceNode node, final VarValue var) {
-//		if (var.isConditionResult()) {
-//			return node.getWrittenVariables().stream().mapToDouble(writtenVar -> writtenVar.getBackwardProb()).average().orElse(PropProbability.UNCERTAIN);
-//		}
-//		
-//		final double totalCost = node.getReadVariables().stream().mapToDouble(readVar -> readVar.computationalCost).sum();
-//		if (totalCost == 0) {
-//			return (1 - node.computationCost) * (1 / node.getReadVariables().size());
-//		} else {
-//			return (1 - node.computationCost) * (var.computationalCost / totalCost);
-//		}
-//	}
 	
 	protected void storeReason(final TraceNode node, final VarValue varValue, final List<ModelPrediction> predictions) {
 		final String reason = this.genReason(predictions);
